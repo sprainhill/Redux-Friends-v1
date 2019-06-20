6f7fe434256db1ed74a1ff8db0919cd021650d2b
Rory Murray <rory.christian.murray@gmail.com>
2019-06-20 17:15:25 -0400

    added navlinks to App.js

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,13 +1,33 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
+import PrivateRoute from "./components/PrivateRoute";
 import FriendViewContainer from "./views/FriendViewContainer";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Rory's Imaginary Friends</h2>
-      <FriendViewContainer />
-    </div>
+    <Router>
+      <div className="wrapper">
+        <ul className="links">
+          <li>
+            <Link to="/login">Log In</Link>
+          </li>
+          <li>
+            <Link to="/FriendViewContainer">Friends List</Link>
+          </li>
+        </ul>
+        <div className="App">
+          <h2>Rory's Imaginary Friends</h2>
+          <Route path="/login" component={Login} />
+          <PrivateRoute
+            exact
+            path="/FriendViewContainer"
+            component={FriendViewContainer}
+          />
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- friends/src/components/FriendCard.js
+++ friends/src/components/FriendCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const FriendCard = props => {
   return (
     <div className="friend-card">
-      <div>Friend here. See? They ARE imaginary.</div>
+      <div>{props.friend.name}</div>
     </div>
   );
 };
